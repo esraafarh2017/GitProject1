1a4633a550369ee904e2608885ee8cbd0580cf3b
Esraa Farh <Aalaabdalguom22@gmail.com>
2021-07-31 21:10:56 -0400

    3 change

--- src/day3/SetPractice.java
+++ src/day3/SetPractice.java
@@ -4,5 +4,6 @@ public class SetPractice {
     public static void main(String[] args) {
         System.out.println("1 change");
         System.out.println("2 change");
+        System.out.println("3 change");
     }
 }
